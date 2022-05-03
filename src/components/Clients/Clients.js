f5128e7d03a68f42e9ea5ca44196127b65009bf9
Mo <mo2005@hotmail.co.uk>
2022-05-03 23:40:30 +0100

    clients: requires redesigning

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -1,9 +1,69 @@
+import "./Clients.css";
+import bianDang from "../../img/biandang.png";
+import pollen from "../../img/pollen.png";
+import sentinel from "../../img/sentinel.png";
+import badgebespoke from "../../img/badgebespoke.jpg";
+import ourClients from "../../img/ourclients.jpg";
+
 const Clients = () => {
-    return ( 
-        <div>
-            Clients
-        </div>
-     );
-}
- 
+  return (
+    <div
+      className="clientsContainer"
+      style={{
+        backgroundImage: `url(${ourClients}`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="clientsContainerChild">
+        <section
+          className="clientItem "
+          style={{
+            backgroundImage: `url(${bianDang}`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+        >
+          1
+        </section>
+        <section
+          className="clientItem "
+          style={{
+            backgroundImage: `url(${pollen}`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+        >
+          2
+        </section>
+        <section
+          className="clientItem "
+          style={{
+            backgroundImage: `url(${sentinel}`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+        >
+          3
+        </section>
+        <section
+          className="clientItem "
+          style={{
+            backgroundImage: `url(${badgebespoke}`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+        >
+          4
+        </section>
+      </div>
+    </div>
+  );
+};
+
 export default Clients;
