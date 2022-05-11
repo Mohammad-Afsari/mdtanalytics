b4f6b52ab8bfcff0bcefb2e1faa0ce2338224f2e
Mo <mo2005@hotmail.co.uk>
2022-05-11 12:49:24 +0100

    restore

---
+++ src/components/Home/Home.js
@@ -0,0 +1,104 @@
+import React from "react";
+import Particles from "react-tsparticles";
+
+const Home = () => {
+    const particlesInit = (main) => {
+        console.log(main);
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+      };
+
+      const particlesLoaded = (container) => {
+        console.log(container);
+      };
+
+    return ( 
+        <div className="home">
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                className="tsparticles"
+                options={{
+                    background: {
+                    color: {
+                        value: "#1f2224",
+                    },
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                    events: {
+                        onClick: {
+                        enable: false,
+                        mode: "push",
+                        },
+                        onHover: {
+                        enable: false,
+                        mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        bubble: {
+                        distance: 400,
+                        duration: 2,
+                        opacity: 0.8,
+                        size: 40,
+                        },
+                        push: {
+                        quantity: 4,
+                        },
+                        repulse: {
+                        distance: 200,
+                        duration: 0.4,
+                        },
+                    },
+                    },
+                    particles: {
+                    color: {
+                        value: "#f5b921",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.3,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outMode: "bounce",
+                        random: false,
+                        speed: 0.2,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                        enable: true,
+                        area: 1500,
+                        },
+                        value: 60,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        random: true,
+                        value: 3,
+                    },
+                    },
+                    detectRetina: true,
+                }}
+            />
+
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/components/Home/Introduction/Introduction.js
@@ -0,0 +1,37 @@
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+// https://codepen.io/Bilal1909/pen/zYqrdRe
+// https://codepen.io/Anon_satyam/pen/JjXLoNJ
+
+const Introduction = () => {
+  return (
+    <div className="introduction">
+      <h1>Relax.</h1>
+      <h4>
+        Let us take the <span>data</span> reins and drive your{" "}
+        <span>business</span> forward.
+      </h4>
+      <h5>
+        We provide all the <span>tools</span>, <span>guidance</span> and{" "}
+        <span>support</span> our <span>clients</span> need to succeed in their
+        respective markets. Take a look around and explore the various{" "}
+        <span>services</span> we offer, the <span>clients</span> that we’ve
+        partnered with, and <span>samples</span> of our work.
+      </h5>
+      <div className="links">
+        <NavLink to="/services" className="nav-logo">
+          <button className="linkBtn services">Services</button>
+        </NavLink>
+
+        <NavLink to="/clients" className="nav-logo">
+          <button className="linkBtn clients">Clients</button>
+        </NavLink>
+
+        <NavLink to="/contact" className="nav-logo">
+          <button className="linkBtn contact">Contact</button>
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
